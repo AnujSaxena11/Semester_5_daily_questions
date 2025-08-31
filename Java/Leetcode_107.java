9635e84b1d4c5f3ed036dd70de5e53bec8430a0b
AnujSaxena11 <anujsaxena337@gmail.com>
2025-08-31 14:54:14 +0530

    Binary Tree Level Order Traversal II

---
+++ Java/Leetcode_107.java
@@ -0,0 +1,39 @@
+ public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        List<List<Integer>> ans = new LinkedList<List<Integer>>();
+        if(root == null){
+            return ans;
+        }
+        q.offer(root);
+        while(!q.isEmpty()){
+            int levelSize = q.size();
+            List<Integer> level = new LinkedList<Integer>();
+            for(int i = 0; i<levelSize; i++){
+                if(q.peek().left != null){
+                    q.offer(q.peek().left);
+                }
+                if(q.peek().right != null){
+                    q.offer(q.peek().right);
+                }
+                level.add(q.poll().val);
+            }
+            ans.add(level);
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
